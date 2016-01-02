464fdd01844def90ebae0df585c5d112d2bedaa5
Constantine Tarasenkov <constantine@protonmail.com>
2016-01-02 11:33:02 +0300

    Make mesh name the same

--- examples/glfw3_opengl3_example/main.c
+++ examples/glfw3_opengl3_example/main.c
@@ -46,10 +46,10 @@ int main()
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
-  GLuint suzanne_bo;
-  glGenBuffers(1, &suzanne_bo);
-  glBindBuffer(GL_ARRAY_BUFFER, suzanne_bo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(suzanne), suzanne, GL_STATIC_DRAW);
+  GLuint mesh_bo;
+  glGenBuffers(1, &mesh_bo);
+  glBindBuffer(GL_ARRAY_BUFFER, mesh_bo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(mesh), mesh, GL_STATIC_DRAW);
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -103,7 +103,7 @@ int main()
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     glUseProgram(program);
-    glDrawArrays(GL_TRIANGLES, 0, sizeof(suzanne) / sizeof(float) / 3);
+    glDrawArrays(GL_TRIANGLES, 0, sizeof(mesh) / sizeof(float) / 3);
 
     glfwSwapBuffers(glfw_window);
   }
--- examples/sdl2_opengl3_example/main.c
+++ examples/sdl2_opengl3_example/main.c
@@ -49,10 +49,10 @@ int main()
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
-  GLuint torus_bo;
-  glGenBuffers(1, &torus_bo);
-  glBindBuffer(GL_ARRAY_BUFFER, torus_bo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(torus), torus, GL_STATIC_DRAW);
+  GLuint mesh_bo;
+  glGenBuffers(1, &mesh_bo);
+  glBindBuffer(GL_ARRAY_BUFFER, mesh_bo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(mesh), mesh, GL_STATIC_DRAW);
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -112,7 +112,7 @@ int main()
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     glUseProgram(program);
-    glDrawArrays(GL_TRIANGLES, 0, sizeof(torus) / sizeof(float) / 3);
+    glDrawArrays(GL_TRIANGLES, 0, sizeof(mesh) / sizeof(float) / 3);
 
     SDL_GL_SwapWindow(sdl_window);
   }
